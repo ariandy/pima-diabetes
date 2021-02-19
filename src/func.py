e444ff6276e82efc086a5fbe5c06a4e58fae1cda
ariandy <ariandynoviar@gmail.com>
2021-02-19 21:16:25 +0700

    First commit

---
+++ src/func.py
@@ -0,0 +1,21 @@
+def iteratee(mode, dataset, dataloader, model, criterion, optimizer, device):
+    if mode == "train":
+        model.train()
+    elif mode == "test":
+        model.eval()
+    cost = correct = 0 
+    for feature, target in dataloader:
+        feature, target = feature.to(device), target.to(device)
+        output = model(feature)
+        loss = criterion(output, target)
+        
+        if mode == "train":
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+        
+        cost += loss.item() * feature.shape[0]
+        correct += (output.argmax(1) == target).sum().item()
+    cost = cost / len(dataset)
+    accuracy = correct / len(dataset)
+    return cost, accuracy
